71442b07cc8cfc3ed12de28e789cfc8b0f492213
Kristen Sharkey <kristen.l.sharkey@gmail.com>
2017-12-25 14:12:44 -0800

    Removed unnecessary email icon from content_results.xml.

--- app/src/main/java/com/kristensharkey/antigen_negative/Results.java
+++ app/src/main/java/com/kristensharkey/antigen_negative/Results.java
@@ -2,8 +2,7 @@ package com.kristensharkey.antigen_negative;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -36,14 +35,7 @@ public class Results extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        //myTextView.setText(resultsNumber);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();*/
 
     }
 
